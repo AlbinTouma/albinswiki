1521d2f13f0e243020d2d855856c75b69d097d1b
albin <albin.touma@gmail.com>
2023-11-30 22:57:07 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,18 @@
 TOKEN=$(cat ~/githubtoken.txt)
 LOG_FILE=deploy_log.txt
 
+# Update the remote URL with the token
+git remote set-url origin "https://AlbinTouma:$TOKEN@github.com/AlbinTouma/albinswiki.git"
+
 fswatch -r .
 
 # Only proceed if there are changes to commit
 if [ "$(git status --porcelain)" ]; then
     echo "Changes detected. Committing and pushing..." >> "$LOG_FILE"
 
-    export GIT_ASKPASS=<(echo "$TOKEN")
     git add .
     git commit -m "Auto commit on change"
     git push origin main
-
-    unset GIT_ASKPASS
 
     echo "Changes committed and pushed successfully." >> "$LOG_FILE"
 else
