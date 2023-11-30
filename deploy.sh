866959098862f892b9919a65a94833a3f6082754
albin <albin.touma@gmail.com>
2023-11-30 22:52:00 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
+TOKEN=$(cat ~/githubtoken.txt)
+LOG_FILE=deploy_log.txt
 
 fswatch -r .
 
-    # Only proceed if there are changes to commit
+# Only proceed if there are changes to commit
 if [ "$(git status --porcelain)" ]; then
+    echo "Changes detected. Committing and pushing..." >> "$LOG_FILE"
+
+    export GIT_ASKPASS=<(echo "$TOKEN")
     git add .
     git commit -m "Auto commit on change"
-    git push
-    break
+    git push origin main
+
+    unset GIT_ASKPASS
+
+    echo "Changes committed and pushed successfully." >> "$LOG_FILE"
+else
+    echo "No changes detected. Exiting." >> "$LOG_FILE"
 fi
 
 exit
+
